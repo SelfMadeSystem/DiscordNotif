abd96ce7cc6427355bc48b981ac8794a0dbacf54
true-cc <creepercannyon@gmail.com>
2020-12-05 09:38:48 -0800

    Added customizable messages and cleaned up code *a bit*.

--- src/main/java/uwu/smsgamer/discordnotif/DiscordNotif.java
+++ src/main/java/uwu/smsgamer/discordnotif/DiscordNotif.java
@@ -7,26 +7,49 @@ import org.bukkit.configuration.file.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.List;
 
+// Yes shit code idc
 public final class DiscordNotif extends JavaPlugin implements CommandExecutor {
 
     public static FileConfiguration config;
     public static boolean papiEnabled;
+    public static String noPermission;
+    public static String noPermissionNotif;
+    public static String basicUsage;
+    public static String errorMessage;
+    public static String notifTypeNotFound;
 
     @Override
     public void onEnable() {
         papiEnabled = Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI");
         getCommand("discordnotif").setExecutor(this);
         loadConfig();
+        noPermission = config.getString("messages.no-permission");
+        noPermissionNotif = config.getString("messages.no-permission-notif");
+        basicUsage = config.getString("messages.basic-usage");
+        errorMessage = config.getString("messages.error-message");
+        notifTypeNotFound = config.getString("messages.notif-type-not-found");
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        OfflinePlayer player = sender instanceof OfflinePlayer ? (OfflinePlayer) sender : ConsolePlayer.getInstance();
         try {
+            if (!sender.hasPermission("discordnotif.command")) {
+                sender.sendMessage(StringHelper.stringify(player, noPermission, args));
+                return true;
+            }
+            if (args.length == 0) {
+                sender.sendMessage(StringHelper.stringify(player, basicUsage, args));
+                return true;
+            }
             ConfigurationSection section = getSection(args[0]);
+            if (section == null) {
+                sender.sendMessage(StringHelper.stringify(player, notifTypeNotFound, args));
+                return true;
+            }
             String sectionName = section.getName();
-            if (sender.hasPermission("discordnotif." + sectionName)) {
-                OfflinePlayer player = sender instanceof OfflinePlayer ? (OfflinePlayer) sender : ConsolePlayer.getInstance();
+            if (sender.hasPermission("discordnotif.notif." + sectionName)) {
                 if (args.length - 1 >= section.getInt("min-args")) {
                     new DiscordWebHook(section.getString("url"),
                       StringHelper.stringifyNoC(player, section.getString("discord-message"), args)).run();
@@ -34,26 +57,25 @@ public final class DiscordNotif extends JavaPlugin implements CommandExecutor {
                 } else {
                     sender.sendMessage(StringHelper.stringify(player, section.getString("usage-message"), args));
                 }
-            } else throw new Exception("No permission");
-        } catch (Exception e) {
-            if (e.getMessage() == null)
-                sender.sendMessage((Arrays.toString(e.getStackTrace()) + "\nReason: " + e.getMessage()).replace("1.12", "1.0").replace("1_12", "1_0"));
-            else if (e.getMessage().toLowerCase().contains("no permission"))
-                sender.sendMessage(("Error. Reason: " + e.getMessage()).replace("1.12", "1.0").replace("1_12", "1_0"));
-            else if (e.getMessage().toLowerCase().contains("no section"))
-                sender.sendMessage(("Error. Reason: " + e.getMessage()));
-            else
-                sender.sendMessage((Arrays.toString(e.getStackTrace()) + "\nReason: " + e.getMessage()).replace("1.12", "1.0").replace("1_12", "1_0"));
+            } else {
+                sender.sendMessage(StringHelper.stringify(player, noPermissionNotif, args));
+            }
+            return true;
+        } catch (Throwable t) {
+            t.printStackTrace();
+            sender.sendMessage(StringHelper.stringify(player, errorMessage, args));
+            return true;
         }
-        return true;
     }
 
-    private ConfigurationSection getSection(String name) throws Exception {
+    private ConfigurationSection getSection(String name) {
         String lowerName = name.toLowerCase();
-        for (String key : config.getKeys(false))
-            if (config.getStringList(key + ".aliases").stream().anyMatch(s -> s.toLowerCase().equals(lowerName)))
+        for (String key : config.getKeys(false)) {
+            List<String> stringList = config.getStringList(key + ".aliases");
+            if (stringList != null && stringList.stream().anyMatch(s -> s.toLowerCase().equals(lowerName)))
                 return config.getConfigurationSection(key);
-        throw new Exception("No section.");
+        }
+        return null;
     }
 
 
